083769cbaf7c2a05acc39952bca3e64010ddbfe9
andre-rondon <rondon@estudiorondon.com.br>
2020-08-12 14:12:44 -0700

    seeds faker made

--- tbc_project_server_side/config/routes.rb
+++ tbc_project_server_side/config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # /api/v1/products
+  # /api/v1/products/:id
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :products, only: [:index, :show, :update, :create, :destroy]
+    end
+  end
+
+  
 end
--- tbc_project_server_side/db/seeds.rb
+++ tbc_project_server_side/db/seeds.rb
@@ -5,3 +5,57 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+Product.delete_all
+User.delete_all
+
+NUM_PRODUCTS = 15
+PASSWORD = '12345'
+
+
+super_user = User.create(
+    first_name: 'jon',
+    last_name: 'snow',
+    email: 'js@winterfell.gov',
+    address: '303 victoria dr'
+    phone: '999999999'
+    password: PASSWORD
+)
+
+NUM_USER.times do
+    first_name = Faker::Name.first_name
+    last_name = Faker::Name.last_name
+    User.create(
+        first_name: first_name,
+        last_name: last_name,
+        email: Faker::Internet.email,
+        password: PASSWORD
+        address: address
+        phone: phone
+    )
+end
+
+PRICE = '15.00'
+
+NUM_PRODUCTS.times do 
+    created_at = Faker::Date.backward(days: 365 * 5)
+    p = Product.create(
+        name: Faker::Hacker.say_something_smart,
+        category: Faker::ChuckNorris.fact,
+        price: PRICE,
+        description: Faker::Hacker.say_something_smart,
+        # user: users.sample, # array method that randomly picks something from an array
+        created_at: created_at,
+        updated_at: created_at
+    ) 
+    end
+end
+
+
+product = Product.all 
+users = User.all
+
+puts Cowsay.say("Generated #{product.count} products", :frogs)
+puts Cowsay.say("Generated #{users.count} users", :sheep)
