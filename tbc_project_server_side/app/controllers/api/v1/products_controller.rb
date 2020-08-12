f7821ecbe764ca1f69294e0a4765b3d65f107b58
andre-rondon <rondon@estudiorondon.com.br>
2020-08-12 08:32:00 -0700

    CRUD API controller from products and user

--- tbc_project_server_side/app/controllers/api/v1/products_controller.rb
+++ tbc_project_server_side/app/controllers/api/v1/products_controller.rb
@@ -1,2 +1,40 @@
 class Api::V1::ProductsController < ApplicationController
+    def index
+        # curl http://localhost:3000/api/v1/
+        products = Product.order(created_at: :desc)
+        render json: products
+      end
+    
+      def show
+         # curl http://localhost:3000/api/v1/products/:id
+        product = Product.find params[:id]
+        render json: products
+      end
+    
+      def create
+        product = Product.new params.require(:product).permit(:name, :category, :description, :price, :img_url )
+        if products.save
+          render json: { id: products.id }
+        else
+          render json: { error: products.errors }
+          status: 422 # unprocessable entitiy HTTP Status Code
+        end
+      end
+
+      def update
+        if product.update product_params
+          render json: { id: product.id }
+        else
+          render(
+            json: { errors: product.errors },
+            status: 422 # Unprocessable Entity
+          )
+        end
+      end
+
+      def destroy
+        product.destroy
+        render(json: { status: 200 }, status: 200)
+      end
 end
+
--- tbc_project_server_side/app/controllers/api/v1/users_controller.rb
+++ tbc_project_server_side/app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,32 @@
 class Api::V1::UsersController < ApplicationController
+    def new
+        @user = User.new
+      end
+    
+      def create
+        @user = User.new params.require(:user).permit(:first_name, :last_name, :email, :password_digest, :password_confirmation, :address, :phone)
+        if @user.save
+          flash.delete(:warning)
+          redirect_to root_path
+        else
+          flash[:warning] = "unable to create user"
+          render :new
+        end
+      end
+
+    
+      def edit
+        # @user User.find session[:user_id]
+        @user = current_user # the current_user method already fetches the logged in user for us
+      end
+
+      private
+
+      def user_params
+        params.require(:user).permit(
+            :first_name, :last_name, :email, :password_digest, :password_confirmation, :address, :phone
+        )
+      end
+    
+     
 end
